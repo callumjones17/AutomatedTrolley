9546c010b2cba23b661c0be7e4f7bed9926ac48e
callumjones17 <s3601120@student.rmit.edu.au>
2018-10-17 07:49:21 +1100

    On the Day

--- main_program.py
+++ main_program.py
@@ -77,34 +77,32 @@ while 1:
     input = serMod.get_serial()
     if input == "2":
         min_distance = objSense.check(_LEFT)
-		# Perhaps do a Serial println Here??
-		#serMod.send_serial(min_distance)
-		if min_distance == 0 or min_distance > max_us_distance:
+		#if min_distance == 0 or min_distance > max_us_distance:
 			print("Going Left")
 			motCont.go_left(1)
-		else:
+		#else:
 			print("Can't Move")
     elif input == "1":
         min_distance = objSense.check(_FORWARD)
-		if min_distance == 0 or min_distance > max_us_distance:
+		#if min_distance == 0 or min_distance > max_us_distance:
 			print("Going Forward")
 			motCont.go_forward(1)
-		else:
+		#else:
 			print("Can't Move");
     elif input == "3":
         min_distance = objSense.check(_RIGHT)
-		if min_distance == 0 or min_distance > max_us_distance:
+		#if min_distance == 0 or min_distance > max_us_distance:
 			print("Going Right")
 			motCont.go_right(1)
-		else:
+		#else:
 			print("Can't Move")
 	# No Sensors on the Back, is this really a good idea??
     elif input == "4":
         min_distance = objSense.check(_BACKWARD))
-		if min_distance == 0 or min_distance > max_us_distance:
+		#if min_distance == 0 or min_distance > max_us_distance:
 			print("Going Backward")
 			motCont.go_backward(2)
-		else:
+		#else:
 			print("Cant Move")
     else:
         print("Stopping")
