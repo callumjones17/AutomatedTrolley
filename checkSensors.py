4e0ce5bc3f9553619a47989c7da6dd6e9b307ead
callumjones17 <s3601120@student.rmit.edu.au>
2018-10-17 07:58:49 +1100

    on Day 5

--- checkSensors.py
+++ checkSensors.py
@@ -23,33 +23,33 @@ right_sensors = ["0110"]#,"0111","1000"]
 
 
 def check_right():
-		min_distance = 0
-        for sensor in right_sensors:
-            mx.selectSensor(sensor, S0, S1, S2, S3, En)
-            distance = (us.runSensor(TRIG,ECHO))
-            if distance < min_distance:
-                min_distance = distance
-            time.sleep(0.01)
+	min_distance = 0
+	for sensor in right_sensors:
+		mx.selectSensor(sensor, S0, S1, S2, S3, En)
+		distance = (us.runSensor(TRIG,ECHO))
+		if distance < min_distance:
+			min_distance = distance
+		time.sleep(0.01)
     return min_distance
 
 def check_left():
-		min_distance = 0
-        for sensor in left_sensors:
-            mx.selectSensor(sensor, S0, S1, S2, S3, En)
-            distance = (us.runSensor(TRIG,ECHO))
-            if distance < min_distance:
-                min_distance = distance
-            time.sleep(0.01)
+	min_distance = 0
+	for sensor in left_sensors:
+		mx.selectSensor(sensor, S0, S1, S2, S3, En)
+		distance = (us.runSensor(TRIG,ECHO))
+		if distance < min_distance:
+			min_distance = distance
+		time.sleep(0.01)
     return min_distance
 
 def check_front():
-		min_distance = 0
-        for sensor in front_sensors:
-            mx.selectSensor(sensor, S0, S1, S2, S3, En)
-            distance = (us.runSensor(TRIG,ECHO))
-            if distance < min_distance:
-                min_distance = distance
-            time.sleep(0.01)
+	min_distance = 0
+	for sensor in front_sensors:
+		mx.selectSensor(sensor, S0, S1, S2, S3, En)
+		distance = (us.runSensor(TRIG,ECHO))
+		if distance < min_distance:
+			min_distance = distance
+		time.sleep(0.01)
     return min_distance
 
 def check_back():
