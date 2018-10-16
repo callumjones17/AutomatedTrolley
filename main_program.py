f20606b2b997496c4bdc6d786169fcdaedd79169
callumjones17 <s3601120@student.rmit.edu.au>
2018-10-17 08:47:05 +1100

    Random

--- main_program.py
+++ main_program.py
@@ -90,23 +90,23 @@ def go_backward():
 
 #Main Loop
 while 1:
-    #input = serMod.get_serial()
+	#input = serMod.get_serial()
 	input_data = input("Direction: ")
-    if input == "left":#"2":
+	if input == "left":#"2":
 		#min_distance = objSense.check(_LEFT)
 		#if min_distance == 0 or min_distance > max_us_distance:
 		print("Going Left")
 		motCont.go_left(1)
 		#else:
 		print("Can't Move")
-    elif input == "forward":#"1":
+	elif input == "forward":#"1":
 		#min_distance = objSense.check(_FORWARD)
 		#if min_distance == 0 or min_distance > max_us_distance:
 		print("Going Forward")
 		motCont.go_forward(1)
 		#else:
 		print("Can't Move");
-    elif input == "right":#3":
+	elif input == "right":#3":
 			#min_distance = objSense.check(_RIGHT)
 		#if min_distance == 0 or min_distance > max_us_distance:
 			print("Going Right")
